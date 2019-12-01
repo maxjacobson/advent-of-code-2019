c84dd6d898b6f9c777f5e174be803d8643c9f6c7
Max Jacobson <max@hardscrabble.net>
2019-12-01 00:26:11 -0500

    Add part one

--- day-01/star-01/src/main.rs
+++ day-01/star-01/src/main.rs
@@ -1,3 +1,13 @@
+// Fuel required to launch a given module is based on its mass. Specifically, to find the fuel
+// required for a module, take its mass, divide by three, round down, and subtract 2.
+fn mass_to_fuel_requirement(mass: f32) -> i32 {
+    (mass / 3.0).floor() as i32 - 2
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = include_str!("../input.txt").lines().fold(0, |acc, line| {
+        acc + mass_to_fuel_requirement(line.parse().unwrap())
+    });
+
+    println!("The result is: {}", result);
 }
