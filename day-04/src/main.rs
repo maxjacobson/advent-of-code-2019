ac4e799f6421bc516468aaf9397e33e3f9a11130
Max Jacobson <max@hardscrabble.net>
2019-12-07 23:52:26 -0500

    Add day 4

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -13,6 +13,12 @@ impl Password {
         self.six_digits() && self.two_adjacent_digits_are_the_same() && self.digits_dont_decrease()
     }
 
+    fn stricter_valid(&self) -> bool {
+        self.six_digits()
+            && self.digits_dont_decrease()
+            && self.exactly_two_adjacent_digits_are_the_same()
+    }
+
     fn six_digits(&self) -> bool {
         self.letters.len() == 6
     }
@@ -22,6 +28,28 @@ impl Password {
             .iter()
             .zip(self.letters.iter().skip(1))
             .any(|(a, b)| a == b)
+    }
+
+    fn exactly_two_adjacent_digits_are_the_same(&self) -> bool {
+        let mut iter = self.letters.iter().peekable();
+        let mut counter = 1;
+
+        loop {
+            if let Some(digit) = iter.next() {
+                if iter.peek() == Some(&digit) {
+                    counter += 1
+                } else {
+                    if counter == 2 {
+                        return true;
+                    }
+                    counter = 1 // reset
+                }
+            } else {
+                break;
+            }
+        }
+
+        false
     }
 
     fn digits_dont_decrease(&self) -> bool {
@@ -37,11 +65,23 @@ fn valid_password(password: &str) -> bool {
     Password::new(password).valid()
 }
 
+fn stricter_valid_password(password: &str) -> bool {
+    Password::new(password).stricter_valid()
+}
+
 fn main() {
     let range = 254032..=789860;
 
-    let count = range.filter(|n| valid_password(&format!("{}", n))).count();
+    let count = range
+        .clone()
+        .filter(|n| valid_password(&format!("{}", n)))
+        .count();
     println!("star 1: {}", count);
+
+    let count = range
+        .filter(|n| stricter_valid_password(&format!("{}", n)))
+        .count();
+    println!("star 2: {}", count);
 }
 
 #[cfg(test)]
@@ -53,5 +93,12 @@ mod tests {
         assert_eq!(valid_password("111111"), true);
         assert_eq!(valid_password("223450"), false);
         assert_eq!(valid_password("123789"), false);
+    }
+
+    #[test]
+    fn test_stricter_valid_password() {
+        assert_eq!(stricter_valid_password("112233"), true);
+        assert_eq!(stricter_valid_password("123444"), false);
+        assert_eq!(stricter_valid_password("111122"), true);
     }
 }
